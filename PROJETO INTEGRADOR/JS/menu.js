16f4de1d7f2dd6547df9c7d5f7a6e4293aecc1ea
GraziMonteiro <monteirograziela385@gmail.com>
2021-12-20 21:02:17 -0300

    Projeto

--- PROJETO INTEGRADOR/JS/menu.js
+++ PROJETO INTEGRADOR/JS/menu.js
@@ -2,11 +2,13 @@ $('.btn').click(function(){
     $(this).toggleClass("click")
     $('.sidebar').toggleClass("show")
 });
-$('.feat-btn').click(function(){
+$('.feat-btn').click(function(event){
+    event.preventDefault();
     $('nav ul .feat-show').toggleClass("show");
     $('nav ul .first').toggleClass("rotate");
 });
-$('.serv-btn').click(function(){
+$('.serv-btn').click(function(event){
+    event.preventDefault();
     $('nav ul .serv-show').toggleClass("show1");
     $('nav ul .second').toggleClass("rotate");
 })
